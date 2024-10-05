a20f96e059c4521e194ab2f47062c5ca2842f36c
tadeowoto <tadeo.woto@gmail.com>
2024-10-05 19:57:12 -0300

    añadiendo los primeros estilos

--- routes/rutaHome.js
+++ routes/rutaHome.js
@@ -3,6 +3,6 @@ import express from "express";
 const router = express.Router();
 
 router.get('/', (req,res)=>{res.render('home')});
-
+router.post('/login') // todo -> hay que hacer el enrutamiento 
 
 export { router };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        secondaryColor: '#18181B', 
+        primaryForeground: '#E4E4E7', 
+        secondaryFont: '#E4E4E7', 
+        background: '#F9FAFB', 
+        input: '#D1D5DB', 
+        ring: '#3B82F6',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
