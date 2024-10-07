bff9ebd7876de9561e5a82fdcf23b15849a8c6b6
tadeowoto <tadeo.woto@gmail.com>
2024-10-06 23:04:59 -0300

    tw config fixeada y estilos terminados en login

--- index.mjs
+++ index.mjs
@@ -1,8 +1,15 @@
 import express from "express";
 import { PORT } from "./config/config.mjs";
 import { router } from "./routes/rutaHome.js";
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url); // Obtiene el nombre del archivo actual
+const __dirname = path.dirname(__filename); // Obtiene el directorio del archivo actual
+
+// Sirve archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'pug');
 app.set('views', './views');
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './views/**/*.pug',
+    './public/**/*.html',
+    './src/**/*.js'
+  ],
   theme: {
     extend: {
       colors: {
         secondaryColor: '#18181B', 
         primaryForeground: '#E4E4E7', 
-        secondaryFont: '#E4E4E7', 
+        secondaryFont: '#717175',
+        fontWhite: '#FFFFFF', 
+        thirdFont: '#1a1a1a',
         background: '#F9FAFB', 
         input: '#D1D5DB', 
         ring: '#3B82F6',
