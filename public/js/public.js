73be5619637a9aec224fb0f1b151f37e8e9287e2
tadeowoto <tadeo.woto@gmail.com>
2024-10-10 13:13:20 -0300

    agregando modales

--- public/js/public.js
+++ public/js/public.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", function (e) {
   var calendarEl = document.getElementById("calendar");
-  
+
   var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: "dayGridMonth", 
-    initialDate: new Date(), 
+    initialView: "dayGridMonth",
+    initialDate: new Date(),
     headerToolbar: {
       left: "prev,next today",
       center: "title",
       right: "",
     },
-    titleFormat: { 
-      month: 'short',
-      year: 'numeric' 
+    titleFormat: {
+      month: "short",
+      year: "numeric",
     },
     dateClick: function (info) {
       var selectedDateEl = info.dayEl;
@@ -21,11 +21,11 @@ document.addEventListener("DOMContentLoaded", function (e) {
       setTimeout(() => {
         selectedDateEl.classList.remove("highlight-day");
       }, 400);
-        
-        fetch(`/agenda/${selectedDate}`, {
-          method: "GET",
-          credentials: "include",
-        })
+
+      fetch(`/agenda/${selectedDate}`, {
+        method: "GET",
+        credentials: "include",
+      })
         .then((response) => response.json())
         .then((agenda) => {
           const turnosContainer = document.getElementById("turnos-container");
@@ -36,16 +36,16 @@ document.addEventListener("DOMContentLoaded", function (e) {
             turnosContainer.appendChild(p);
             return;
           }
-          // esto crea el carticulo de la agenda  
+          // esto crea el carticulo de la agenda
           agenda.forEach((turno) => {
             const article = document.createElement("article");
             article.className =
-            "w-full flex items-center justify-between border-b pb-2";
+              "w-full flex items-center justify-between border-b pb-2";
             const divInfo = document.createElement("div");
             divInfo.className =
-            "flex items-center justify-center min-w-36 max-w-full gap-3";
-            
-            const divHora = document.createElement('div');
+              "flex items-center justify-center min-w-36 max-w-full gap-3";
+
+            const divHora = document.createElement("div");
             divHora.className = "w-20 flex items-center justify-center gap-2";
             const svgClock = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clock h-5 w-5 text-gray-500"><circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline></svg>`;
             divHora.innerHTML += svgClock;
@@ -53,39 +53,66 @@ document.addEventListener("DOMContentLoaded", function (e) {
               .split(":")
               .slice(0, 2)
               .join(":")}</p>`;
-              
-              const divNyM = document.createElement('div');
-              divNyM.className = "w-26 flex items-center justify-center gap-2 "
-              const svgUser = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user h-5 w-5 text-gray-500"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>`;
-              const svgMotivo = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-text h-5 w-5 text-gray-500" data-id="21"><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"></path><path d="M14 2v4a2 2 0 0 0 2 2h4"></path><path d="M10 9H8"></path><path d="M16 13H8"></path><path d="M16 17H8"></path></svg>`;
-              divNyM.innerHTML += svgUser;
-              divNyM.innerHTML += `<p >${turno.nombre}</p>`;
-              divNyM.innerHTML += svgMotivo;
-              divNyM.innerHTML += `<p >${turno.motivo}</p>`;
-              divInfo.appendChild(divHora);
-              divInfo.appendChild(divNyM);
-              article.appendChild(divInfo);
-              
-              const divActions = document.createElement("div");
-              divActions.className =
+
+            const divNyM = document.createElement("div");
+            divNyM.className = "w-26 flex items-center justify-center gap-2 ";
+            const svgUser = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user h-5 w-5 text-gray-500"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>`;
+            const svgMotivo = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-text h-5 w-5 text-gray-500" data-id="21"><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"></path><path d="M14 2v4a2 2 0 0 0 2 2h4"></path><path d="M10 9H8"></path><path d="M16 13H8"></path><path d="M16 17H8"></path></svg>`;
+            divNyM.innerHTML += svgUser;
+            divNyM.innerHTML += `<p >${turno.nombre}</p>`;
+            divNyM.innerHTML += svgMotivo;
+            divNyM.innerHTML += `<p >${turno.motivo}</p>`;
+            divInfo.appendChild(divHora);
+            divInfo.appendChild(divNyM);
+            article.appendChild(divInfo);
+
+            const divActions = document.createElement("div");
+            divActions.className =
               "flex items-center justify-center min-w-36 max-w-52 gap-5";
-              
-              if (turno.estado === "pendiente") {
-                divActions.innerHTML += `<p class="px-2 py-1 rounded-full text-xs bg-blue-100 text-blue-800">Pendiente</p>`;
-                divActions.innerHTML += `<button class="inline-flex items-center justify-center text-sm font-medium transition-colors border border-input bg-transparent hover:bg-primaryForeground/5 h-9 rounded-md px-3">Abrir Consulta</button>`;
-              } else {
-                divActions.innerHTML += `<p class="px-2 py-1 rounded-full text-xs bg-green-100 text-green-800">Atendido</p>`;
-                divActions.innerHTML += `<button class="inline-flex items-center justify-center text-sm font-medium transition-colors border border-input bg-transparent hover:bg-primaryForeground/5 h-9 rounded-md px-3">Ver HCE</button>`;
-              }
-              
-              article.appendChild(divActions);
-              turnosContainer.appendChild(article);
-            });
-          })
-          .catch((error) => {
-            console.error("Error al cargar la agenda:", error);
+
+            if (turno.estado === "pendiente") {
+              divActions.innerHTML += `<p class="px-2 py-1 rounded-full text-xs bg-blue-100 text-blue-800">Pendiente</p>`;
+              divActions.innerHTML += `<button class="inline-flex items-center justify-center text-sm font-medium transition-colors border border-input bg-transparent hover:bg-primaryForeground/5 h-9 rounded-md px-3">Abrir Consulta</button>`;
+            } else {
+              divActions.innerHTML += `<p class="px-2 py-1 rounded-full text-xs bg-green-100 text-green-800">Atendido</p>`;
+              divActions.innerHTML += `<button class="inline-flex items-center justify-center text-sm font-medium transition-colors border border-input bg-transparent hover:bg-primaryForeground/5 h-9 rounded-md px-3">Ver HCE</button>`;
+            }
+
+            article.appendChild(divActions);
+            turnosContainer.appendChild(article);
           });
-        },
-      });
-      calendar.render();
+        })
+        .catch((error) => {
+          console.error("Error al cargar la agenda:", error);
+        });
+    },
+  });
+  calendar.render();
+
+  const modal = document.getElementById("modalPaciente");
+  const closeModal = document.getElementById("closeModal");
+  const patientInfo = document.getElementById("infoPaciente");
+
+  document.querySelectorAll(".modalPaciente").forEach((item) => {
+    item.addEventListener("click", function (e) {
+      e.preventDefault();
+      patientInfo.textContent = this.getAttribute("data-info");
+      modal.classList.remove("hidden");
+      modal.classList.add("flex");
+    });
+  });
+
+  closeModal.addEventListener("click", function () {
+    modal.classList.add("hidden");
+    modal.classList.remove("flex");
+  });
+
+  window.addEventListener("click", function (e) {
+    if (e.target === modal) {
+      modal.classList.add("hidden");
+      modal.classList.remove("flex");
+    }
+  });
+
 });
+
