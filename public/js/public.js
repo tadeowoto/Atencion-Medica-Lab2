2ecd3a7d366294a2366d3c6b2aa805a86c025b94
tadeowoto <tadeo.woto@gmail.com>
2024-10-10 12:34:01 -0300

    fixeando estilos

--- public/js/public.js
+++ public/js/public.js
@@ -46,16 +46,16 @@ document.addEventListener("DOMContentLoaded", function (e) {
             "flex items-center justify-center min-w-36 max-w-full gap-3";
             
             const divHora = document.createElement('div');
-            divHora.className = "w-20 flex items-center justify-center ";
+            divHora.className = "w-20 flex items-center justify-center gap-2";
             const svgClock = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clock h-5 w-5 text-gray-500"><circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline></svg>`;
             divHora.innerHTML += svgClock;
-            divHora.innerHTML += `<p class="ml-2 mr-5">${turno.inicio
+            divHora.innerHTML += `<p>${turno.inicio
               .split(":")
               .slice(0, 2)
               .join(":")}</p>`;
               
               const divNyM = document.createElement('div');
-              divNyM.className = "w-26 flex items-center justify-center gap-2"
+              divNyM.className = "w-26 flex items-center justify-center gap-2 "
               const svgUser = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user h-5 w-5 text-gray-500"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>`;
               const svgMotivo = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-text h-5 w-5 text-gray-500" data-id="21"><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"></path><path d="M14 2v4a2 2 0 0 0 2 2h4"></path><path d="M10 9H8"></path><path d="M16 13H8"></path><path d="M16 17H8"></path></svg>`;
               divNyM.innerHTML += svgUser;
